b4c9934f1f3994788c743892af6bca99baf6a495
Sachin Natesh <sachin.natesh@gmail.com>
2019-04-19 18:02:54 -0400

    Update animate.sh

--- VOF_Extract_Field_at_Interface/animate.sh
+++ VOF_Extract_Field_at_Interface/animate.sh
@@ -5,9 +5,10 @@
 # get from apt by 'sudo apt install mencoder'. Mencoder is used to 
 # transcode a set of png files into an avi movie.
 
+# change w (width), h (height), fps (frames per second,) o (output name) as needed.
+
 # create file list
 ls -lrt *.png | awk '{print $9}' > file_list
 
 # uses mencoder to animate png frames into avi
 mencoder mf://@file_list -mf w=800:h=600:fps=5:type=png -ovc copy -oac copy -o output.avi
-
