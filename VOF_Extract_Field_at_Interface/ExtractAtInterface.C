590bb0c7b5fdb5cbc2f5f47271d247809ffef479
Sachin Natesh <srn324@jupiter.cims.nyu.edu>
2019-06-13 18:15:27 -0400

    gutting plot feature, added smoothing for radial extract

--- VOF_Extract_Field_at_Interface/ExtractAtInterface.C
+++ VOF_Extract_Field_at_Interface/ExtractAtInterface.C
@@ -46,6 +46,8 @@ struct Args
   std::string title;
   int width;
   int height;
+	bool plot;
+	bool smooth;
 };
 
 // construct Args class from input json file
@@ -71,41 +73,52 @@ Args readJSON(jsoncons::json inputjson)
   jsoncons::json contouropt = inputjson["Contour"];
   args.contourArray = contouropt["array"].as<std::string>();
   args.contour_val = contouropt["value"].as<double>();
-  args.title = inputjson["Plot"]["title"].as<std::string>();
-  args.width = inputjson["Plot"]["width"].as<int>();
-  args.height = inputjson["Plot"]["height"].as<int>();
-  // optional
-  if (contouropt.has_key("xmin"))
-    args.xmin = contouropt["xmin"].as<double>();
-  else
-    args.xmin = INFINITY;
-  if (contouropt.has_key("xmax"))
-    args.xmax = contouropt["xmax"].as<double>();
-  else
-    args.xmax = INFINITY;
-  if (contouropt.has_key("ymin"))
-    args.ymin = contouropt["ymin"].as<double>();
-  else
-    args.ymin = INFINITY;
-  if (contouropt.has_key("ymax"))
-    args.ymax = contouropt["ymax"].as<double>();
-  else
-    args.ymax = INFINITY;
+  args.write = inputjson["Write"].as<bool>();
+	
+	args.smooth = (args.dimension == 2 ? 0 : inputjson["Smoothing"].as<bool>());
+	args.plot = false; 
+  if (inputjson.has_key("Plot"))
+	{
+		args.plot = true;
+		args.title = inputjson["Plot"]["title"].as<std::string>();
+  	args.width = inputjson["Plot"]["width"].as<int>();
+  	args.height = inputjson["Plot"]["height"].as<int>();
+  	// optional
+  	if (contouropt.has_key("xmin"))
+  	  args.xmin = contouropt["xmin"].as<double>();
+  	else
+  	  args.xmin = INFINITY;
+  	if (contouropt.has_key("xmax"))
+  	  args.xmax = contouropt["xmax"].as<double>();
+  	else
+  	  args.xmax = INFINITY;
+  	if (contouropt.has_key("ymin"))
+  	  args.ymin = contouropt["ymin"].as<double>();
+  	else
+  	  args.ymin = INFINITY;
+  	if (contouropt.has_key("ymax"))
+  	  args.ymax = contouropt["ymax"].as<double>();
+  	else
+  	  args.ymax = INFINITY;
+	}
   if (contouropt.has_key("data"))
   {
     args.dataOnContour = contouropt["data"]["name"].as<std::string>();
-    if (contouropt["data"].has_key("ymin"))
-      args.dymin = contouropt["data"]["ymin"].as<double>();
-    else
-      args.dymin = INFINITY;
-    if (contouropt["data"].has_key("ymax"))
-      args.dymax = contouropt["data"]["ymax"].as<double>();
-    else
-      args.dymax = INFINITY;
-  }
+    if (args.plot)
+		{
+			if (contouropt["data"].has_key("ymin"))
+    	  args.dymin = contouropt["data"]["ymin"].as<double>();
+    	else
+    	  args.dymin = INFINITY;
+    	if (contouropt["data"].has_key("ymax"))
+    	  args.dymax = contouropt["data"]["ymax"].as<double>();
+    	else
+    	  args.dymax = INFINITY;
+  	}
+	}
   else
     args.dataOnContour = "";
-  args.write = inputjson["Write"].as<bool>(); 
+
   return args;
 }
 
@@ -133,94 +146,136 @@ int main(int argc, char* argv[])
     ContourInterface::Create(args.caseName,args.caseType,args.contourArray,args.contour_val,dataName);
 
   double nT = (args.end-args.beg)/args.stride;
-  plt::figure();
-  plt::figure_size(args.width,args.height);
-  for (int i = 0; i <= nT; ++i)
+  if (args.plot)
+	{
+		plt::figure();
+  	plt::figure_size(args.width,args.height);
+  }
+	for (int i = 0; i <= nT; ++i)
   {
-    double time = i*args.stride+args.beg;
+  	double time = i*args.stride+args.beg;
+		std::cout << "Countouring at Time = " << time << std::endl;
     contour->stepTo(time);
-    std::stringstream ss; 
-    ss << "Time" << std::internal << setw(5) << setfill('0') << time << ".png"; 
-    std::string figName(trim_fname(args.caseName,ss.str())); 
-    std::vector<double> heights, contourData, axis;
+    std::string figName;
+		if (args.plot)
+		{
+			std::stringstream ss; 
+    	ss << "Time" << std::internal << setw(5) << setfill('0') << time << ".png"; 
+    	figName = trim_fname(args.caseName,ss.str()); 
+    }
+		std::vector<double> heights, smoothHeights, contourData, axis, smoothAxis;
     if (!args.dataOnContour.empty())
-    {
+		{
       contour->getContour(heights,contourData,axis);
-      // define names for plt legend
-      plt::clf();
-      plt::subplot(2,1,1);
-      plt::named_plot("height (m)", axis, heights,"b-");
-      if (!(isinf(args.xmin)  || isinf(args.xmax)))
-        plt::xlim(args.xmin,args.xmax);
-      if (!(isinf(args.ymin) || isinf(args.ymax)))
-        plt::ylim(args.ymin,args.ymax);
-      plt::legend();
-      std::stringstream ss; 
-      ss << args.title << ", Contour by "<<args.contourArray<<"="<<
-            args.contour_val<<", Time: "<<time;
-      std::string titleText(ss.str());
-      plt::title(titleText);
-      plt::grid(true);
-      plt::subplot(2,1,2);
-      plt::named_plot(args.dataOnContour, axis, contourData, "k-");
-      if (!(isinf(args.xmin)  || isinf(args.xmax)))
-        plt::xlim(args.xmin,args.xmax);
-      if (!(isinf(args.dymin) || isinf(args.dymax)))
-        plt::ylim(args.dymin,args.dymax);
-      plt::grid(true);  
-      plt::xlabel("x (m)");
-      plt::legend();
-      plt::save(figName);
-      plt::pause(0.0001);
-    }
-    else
-    {
-      std::string marker;
-      if (args.dimension == 2)
-      {
-        contour->getContour(heights, axis);
-        marker = "b.-";
-      }
-      else
-      {
-        contour->getRadialContour(heights,axis);
-        marker = "b.";
-      }
-      plt::clf();
-      plt::named_plot("height (m)", axis, heights,marker);
-      if (!(isinf(args.xmin) || isinf(args.xmax)))
-        plt::xlim(args.xmin,args.xmax);
-      if (!(isinf(args.ymin) || isinf(args.ymax)))
-        plt::ylim(args.ymin,args.ymax);
-      plt::legend();
-      std::stringstream ss; 
-      ss << args.title << ", Contour by "<< args.contourArray <<"=" <<
-            args.contour_val<<", Time: "<< time;
-      std::string titleText(ss.str());
-      plt::title(titleText);
-      plt::grid(true);
-      
-      args.dimension == 2 ? plt::xlabel("x (m)") : plt::xlabel("r (m)");
-      plt::save(figName);
-      plt::pause(0.0001);
-    }
-    if (args.write)
-    {
-      std::stringstream ss;
-      ss << "OpenFoamframeNo" << std::internal << setw(5) << setfill('0') <<  i+1 << ".txt";
-      std::ofstream outputStream(ss.str());
-      if (!outputStream.good())
-      {
-        std::cerr << "Error creating file " << ss.str() << std::endl;
-        exit(1);
-      }
-      for (int i = 0; i  < heights.size(); ++i)
-      {
-        outputStream << axis[i] << " ";
-        outputStream << heights[i] << std::endl;
-      }
-      outputStream.close();
-    }
-  } 
+			if (args.plot)
+			{
+		  	// define names for plt legend
+      	plt::clf();
+      	plt::subplot(2,1,1);
+      	plt::named_plot("height (m)", axis, heights,"b-");
+      	if (!(isinf(args.xmin)  || isinf(args.xmax)))
+      	  plt::xlim(args.xmin,args.xmax);
+      	if (!(isinf(args.ymin) || isinf(args.ymax)))
+      	  plt::ylim(args.ymin,args.ymax);
+      	plt::legend();
+      	std::stringstream ss; 
+      	ss << args.title << ", Contour by "<<args.contourArray<<"="<<
+      	      args.contour_val<<", Time: "<<time;
+      	std::string titleText(ss.str());
+      	plt::title(titleText);
+      	plt::grid(true);
+      	plt::subplot(2,1,2);
+      	plt::named_plot(args.dataOnContour, axis, contourData, "k-");
+      	if (!(isinf(args.xmin)  || isinf(args.xmax)))
+      	  plt::xlim(args.xmin,args.xmax);
+      	if (!(isinf(args.dymin) || isinf(args.dymax)))
+      	  plt::ylim(args.dymin,args.dymax);
+      	plt::grid(true);  
+      	plt::xlabel("x (m)");
+      	plt::legend();
+      	plt::save(figName);
+     		// plt::pause(0.0001);
+    	}
+		}
+		else
+ 	 	{
+			std::string marker;
+ 	   	if (args.dimension == 2)
+ 	   	{
+ 	   	  contour->getContour(heights, axis);
+ 	   	  marker = "b.-";
+ 	   	}
+ 	   	else
+ 	   	{
+				if (!args.smooth)
+ 	   			contour->getRadialContour(heights,axis);
+				else
+				{
+					contour->getSmoothRadialContour(heights,smoothHeights,axis,smoothAxis);
+				}
+ 	   	  marker = "b.";
+ 	   	}
+			if (args.plot)
+			{
+ 	 			plt::clf();
+ 	  	 	plt::subplot(2,1,1);
+ 	 			plt::named_plot("height (m)", axis, heights,marker);
+ 	  	 	if (!(isinf(args.xmin) || isinf(args.xmax)))
+ 	  	 	  plt::xlim(args.xmin,args.xmax);
+ 	  	 	if (!(isinf(args.ymin) || isinf(args.ymax)))
+ 	  	 	  plt::ylim(args.ymin,args.ymax);
+ 	  	 	plt::legend();
+ 	  	 	std::stringstream ss; 
+ 	  	 	ss << args.title << ", Contour by "<< args.contourArray <<"=" <<
+ 	  	 	      args.contour_val<<", Time: "<< time;
+ 	  	 	std::string titleText(ss.str());
+ 	  	 	plt::title(titleText);
+ 	  	 	plt::grid(true);
+ 	  	 	plt::subplot(2,1,2);
+ 	 			plt::named_plot("height (m)", axis, heights,marker);
+ 	 			if (!(isinf(args.xmin) || isinf(args.xmax)))
+ 	  	 	  plt::xlim(args.xmin,args.xmax);
+ 	  	 	plt::ylim(-500,500);
+ 	  	 	plt::legend();
+ 	  	 	args.dimension == 2 ? plt::xlabel("x (m)") : plt::xlabel("r (m)");
+ 	  	 	plt::save(figName);
+ 	  	 	//plt::pause(1000.);
+ 	 		}
+ 	 	}
+		if (args.write)
+ 	 	{
+ 	   	std::stringstream ss;
+ 	   	ss << "OpenFoamframeNo" << std::internal << setw(5) << setfill('0') <<  i+1 << ".txt";
+			std::ofstream outputStream(ss.str());
+ 	   	if (!outputStream.good())
+ 	   	{
+ 	   	  std::cerr << "Error creating file " << ss.str() << std::endl;
+ 	   	  exit(1);
+ 	   	}
+ 	   	for (int i = 0; i  < heights.size(); ++i)
+ 	   	{
+ 	   	  outputStream << axis[i] << " ";
+ 	   	  outputStream << heights[i] << std::endl;
+ 	   	}
+ 	   	outputStream.close();
+ 	   	if (args.smooth)
+			{
+				ss.str("");
+				ss << "OpenFoamSmoothFrameNo" << std::internal << setw(5) << setfill('0') <<  i+1 << ".txt";
+				outputStream.open(ss.str());
+ 	   		if (!outputStream.good())
+ 	   		{
+ 	   		  std::cerr << "Error creating file " << ss.str() << std::endl;
+ 	   		  exit(1);
+ 	   		}
+
+				for (int i = 0; i < smoothHeights.size(); ++i)
+				{
+					outputStream << smoothAxis[i] << " ";
+					outputStream << smoothHeights[i] << std::endl;
+				}
+			}
+		}
+	}
   return 0;
 }
--- VOF_Extract_Field_at_Interface/src/ContourInterface.C
+++ VOF_Extract_Field_at_Interface/src/ContourInterface.C
@@ -1,4 +1,8 @@
 #include<ContourInterface.H>
+#include<vtkPoints.h>
+#include<vtkPointLocator.h>
+#include<vtkIdList.h>
+#include<algorithm>
 #include<map>
 
 ContourInterface::ContourInterface(const std::string& _caseName, int _caseType, 
@@ -27,7 +31,7 @@ void ContourInterface::initReader()
   reader->SetCaseType(caseType);
   reader->SetFileName(caseName.c_str());
   reader->ListTimeStepsByControlDictOn();
-  reader->SkipZeroTimeOn();
+  reader->SkipZeroTimeOff();
   reader->Update();
   bool arrayExists = false; 
   bool dataArrayExists = false; 
@@ -70,9 +74,10 @@ void ContourInterface::stepTo(double time)
 {
   // update to current time step
   reader->UpdateTimeStep(time);
-  // interpolate cell centered data to vertices
+  // NOT interpolating cell centered data to vertices
+  //reader->CreateCellToPointOff();
   reader->CreateCellToPointOn();
-  reader->Update();
+	reader->Update();
   // pull out grid
   vtkUnstructuredGrid * currMesh = 
     vtkUnstructuredGrid::SafeDownCast(reader->GetOutput()->GetBlock(0));
@@ -86,13 +91,20 @@ void ContourInterface::getContour(std::vector<double>& heights,
                                   std::vector<double>& xaxis)
 {
   contourFilter->SetValue(0,contour_val);
+	contourFilter->UseScalarTreeOn();
   contourFilter->Update();
   vtkSmartPointer<vtkPolyData> polys = contourFilter->GetOutput();
+  //vtkSmartPointer<vtkCellCenters> cellCenters 
+	//	= vtkSmartPointer<vtkCellCenters>::New();
+	//cellCenters->SetInputData(polys);
+	//cellCenters->Update();
+	//int numPoints = cellCenters->GetOutput()->GetNumberOfPoints();
   int numPoints = polys->GetNumberOfPoints();
-  std::map<double,double> axisHeightMap;
+	std::map<double,double> axisHeightMap;
   for (int j = 0; j < numPoints; ++j)
   {
     double point[3];
+    //cellCenters->GetOutput()->GetPoint(j,point);
     polys->GetPoint(j,point);
     if (point[2] == 0)
     {
@@ -113,20 +125,143 @@ void ContourInterface::getContour(std::vector<double>& heights,
 void ContourInterface::getRadialContour(std::vector<double>& heights, std::vector<double>& raxis)
 {
   contourFilter->SetValue(0,contour_val);
+	contourFilter->UseScalarTreeOn();
   contourFilter->Update();
   vtkSmartPointer<vtkPolyData> polys = contourFilter->GetOutput();
-  int numPoints = polys->GetNumberOfPoints();
-  std::map<double,double> axisHeightMap;
-  heights.resize(numPoints); raxis.resize(numPoints);
+  //vtkSmartPointer<vtkCellCenters> cellCenters 
+	//	= vtkSmartPointer<vtkCellCenters>::New();
+	//cellCenters->SetInputData(polys);
+	//cellCenters->Update();
+	//int numPoints = cellCenters->GetOutput()->GetNumberOfPoints();
+	int numPoints = polys->GetNumberOfPoints();
+	heights.resize(numPoints); raxis.resize(numPoints);
   for (int j = 0; j < numPoints; ++j)
   {
     double point[3];
+    //cellCenters->GetOutput()->GetPoint(j,point);
     polys->GetPoint(j,point);
-    heights[j] = point[1];
-    raxis[j] = std::sqrt(std::pow(point[0],2)+std::pow(point[2],2)); 
-  }
+  	//if(point[0] < 0.16 || point[2] < 0.16 || std::abs(point[0]-point[2]) < 0.32)
+		//{ 
+	 		heights[j] = point[1];
+			//std::cout << point[1] << std::endl;
+    	//heights.push_back(point[1]);
+			//raxis.push_back(std::sqrt(std::pow(point[0],2)+std::pow(point[2],2)));
+			raxis[j] = std::sqrt(std::pow(point[0],2)+std::pow(point[2],2)); 
+  	//}
+	}
 }    
 
+// contour by contour_val, put inverse weighted distance average of y coordinates
+// at given radial distance, with respect to mean of coordinates, into heights
+// then, do another round of smoothing on the heights array wrt nearest neighbors
+// in (r,h) space. lastly, r= sqrt(x^2+z^2) coordinates go into raxis
+// NOTE: Most of the if blocks are for preventing NAN values
+void ContourInterface::getSmoothRadialContour(std::vector<double>& heights, 
+																							std::vector<double>& smoothHeights,
+																							std::vector<double>& raxis,
+																							std::vector<double>& raxisSort)
+{
+  contourFilter->SetValue(0,contour_val);
+	contourFilter->UseScalarTreeOn();
+  contourFilter->Update();
+  vtkSmartPointer<vtkPolyData> polys = contourFilter->GetOutput();
+	int numPoints = polys->GetNumberOfPoints();
+	heights.resize(numPoints); raxis.resize(numPoints);
+	std::map<double,std::vector<double>> raxisHeightMap;
+	for (int j = 0; j < numPoints; ++j)
+  {
+    double point[3];
+    polys->GetPoint(j,point);
+	 	heights[j] = point[1];
+		raxis[j] = std::sqrt(std::pow(point[0],2)+std::pow(point[2],2)); 
+		raxisHeightMap[raxis[j]].push_back(heights[j]);
+	}
+  smoothHeights.resize(raxisHeightMap.size());
+	raxisSort.resize(smoothHeights.size());
+  int j = 0;
+	std::cout << "Smoothing mulivalued radial contour" << std::endl;
+  for (auto it = raxisHeightMap.begin(); it != raxisHeightMap.end(); ++it)
+  {
+		// get average height at this radius
+  	double heightsAve = 0;
+		for (int i = 0; i < it->second.size(); ++i)
+		{
+			heightsAve += it->second[i];
+		}  
+		heightsAve /= it->second.size();
+		// get inverse distance weights relative to average
+    double totWeight = 0;
+		std::vector<double> hInd;
+		for (int i = 0; i < it->second.size(); ++i)
+		{
+			if (!(std::abs(it->second[i] -heightsAve) < 1e-15))
+			{
+				totWeight += 1./(std::abs(it->second[i]-heightsAve));
+				hInd.push_back(i);
+			}
+		}
+		if (!totWeight)
+		{
+			smoothHeights[j] = it->second[0];
+		}	
+		else
+		{
+			// calculate inverse distance weighted average of height
+			smoothHeights[j] = 0;
+			for (int i = 0; i < hInd.size(); ++i)
+			{
+				smoothHeights[j] 
+					+= it->second[hInd[i]]*1./(std::abs(it->second[hInd[i]]-heightsAve));
+			}
+			smoothHeights[j] /= totWeight;
+		}
+		raxisSort[j] = it->first;
+		j = j + 1;
+  }
+	// rounds of smoothing in (r,h) space
+	int nCycles = 5;
+	std::cout << "Smoothing radial contour in (r,h) space for " << nCycles 
+						<< " cycles" << std::endl;
+	// do several rounds of inverse distance averaging in (r,h) space with 
+	// nearest nNeighbr points to ith radius. This is like convolving a 
+	// weighted symmetric box over the scattered heights, starting nNeighbrs deep 
+	// into the heights array
+	for (int cycle = 1; cycle <= nCycles; ++cycle)
+	{
+		std::cout << "\t cycle = " << cycle << std::endl;
+
+		int nNeighbrs = 25;
+		int lo = (int) nNeighbrs - std::ceil(nNeighbrs/2);	
+		int up = (int) nNeighbrs + std::floor(nNeighbrs/2);
+		int end = raxisSort.size() - std::floor(nNeighbrs/2);
+		for (int i = nNeighbrs; i < end; ++i)
+		{
+			double totWeight = 0;
+			std::vector<double> hInd;
+			for (int j = lo; j < up; ++j)
+			{
+				double dist = std::abs(raxisSort[j]-raxisSort[i]);
+				if (!(dist < 1e-15))
+				{
+					totWeight += 1./dist;
+					hInd.push_back(j);
+				}
+			}
+			if (totWeight > 1e-15)
+			{
+				smoothHeights[i] = 0;
+				for (int j = 0; j < hInd.size(); ++j)
+				{
+					double dist = std::abs(raxisSort[i] - raxisSort[hInd[j]]);
+					smoothHeights[i] += smoothHeights[hInd[j]]*1./dist;
+				}
+				smoothHeights[i] /= totWeight;
+			}
+			lo += 1;
+			up += 1;
+		}
+	}
+}    
 // contour by contour_val, put y coords into heights, 
 // data with name dataName into datas and x coords into xaxis
 void ContourInterface::getContour(std::vector<double>& heights, 
@@ -134,6 +269,7 @@ void ContourInterface::getContour(std::vector<double>& heights,
                                   std::vector<double>& xaxis)
 {
   contourFilter->SetValue(0,contour_val);
+	contourFilter->UseScalarTreeOn();
   contourFilter->Update();
   vtkSmartPointer<vtkPolyData> polys = contourFilter->GetOutput();
   vtkSmartPointer<vtkPointData> pd = polys->GetPointData();
@@ -172,6 +308,7 @@ void ContourInterface::getContour(std::vector<double>& heights,
                                   std::vector<double>& xaxis)
 {
   contourFilter->SetValue(0,contour_val);
+	contourFilter->UseScalarTreeOn();
   contourFilter->Update();
   vtkSmartPointer<vtkPolyData> polys = contourFilter->GetOutput();
   vtkSmartPointer<vtkPointData> pd = polys->GetPointData();
